158b2579925496103a435116c75c307945b2b72c
liupu14 <lpgauss@163.com>
2020-01-16 21:23:32 +0800

    add quicksort in sortAlgorithm

--- Python/sortAlgorithm.py
+++ Python/sortAlgorithm.py
@@ -32,3 +32,18 @@ class sortAlgorithm():
                 self.L[ii+1] = Temp 
         return self.L 
 
+
+    def quicksort(self,left,right):
+        if left < right:
+            q = sortAlgorithm.partial(self.L,left,right)
+            quicksort(self.L,left,q-1)
+            quicksort(self.L,q+1,right)
+
+    def partial(self,left,right):
+        x = self.L[right]
+        ii = left - 1
+        for jj in range(left,right):
+            if self.L[jj] <= x:
+                self.L[ii],self.L[jj] = self.L[jj],self.L[ii]
+        self.L[ii+1],self.L[right] = self.L[right],self.L[ii+1]
+        return ii+1

e21a9dab79a7d879059effdfcf6d5ec7e1c96b80
liupu14 <lpgauss@163.com>
2020-02-06 15:42:49 +0800

    add Dijkstra function in graphAlgorthm file

--- Python/graphAlgorithm.py
+++ Python/graphAlgorithm.py
@@ -31,4 +31,48 @@ def search(name):
                 search_deque += graph[person]
     return None 
 
-     
+# ====================================== #
+# =========Dijkstra algorithm=========== #
+# ====================================== #
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"] = {}
+graph["a"]["final"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["final"] = 5
+graph["final"] = {}
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["final"] = infinity 
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["final"] = None 
+proceesed = []
+def find_lowest_cost_nodes(costs):
+    lowest_cost = infinity 
+    lowest_cost_node =None 
+    for node in costs: 
+        cost = costs[node] 
+        if cost < lowest_cost and node is not proceesed: 
+            lowest_cost = cost 
+            lowest_cost_node = node 
+    return lowest_cost_node 
+
+node = find_lowest_cost_nodes(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost 
+            parents[n] = node 
+    proceesed.append(node) 
+    node = find_lowest_cost_nodes(costs)
+
